bb5dd4d1cd1b63cc5410ceb542f7f4c165c787ed
CarriazaL <carlosarriaza38@gmail.com>
2026-07-24 13:12:41 -0600

    Feat: Cambio a .Net 9

--- Plantillas/Plantilla_Cliente/Butacas.cs
+++ Plantillas/Plantilla_Cliente/Butacas.cs
@@ -14,5 +14,10 @@ namespace Plantilla_Cliente
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
