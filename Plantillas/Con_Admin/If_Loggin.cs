e5412842b7fb6cc574ece6275839b52e702b0eb8
Diego Santizo <df.santizo7@gmail.com>
2026-07-31 17:08:32 -0600

    Interfaz de Peliculas terminada por el momento. Nuevas interfaces creadas

---
+++ Plantillas/Con_Admin/If_Loggin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Con_Admin
+{
+    internal class If_Loggin
+    {
+    }
+}
