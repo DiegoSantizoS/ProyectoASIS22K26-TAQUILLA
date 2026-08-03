d5d2e3ce2148c7932e189bdb73137221413217b2
Diego Santizo <df.santizo7@gmail.com>
2026-08-02 21:44:19 -0600

    Arreglo de Login

---
+++ Plantillas/testing/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace testing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            kryptonComboBox3 = new Krypton.Toolkit.KryptonComboBox();
+            kryptonComboBox1 = new Krypton.Toolkit.KryptonComboBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            kryptonComboBox2 = new Krypton.Toolkit.KryptonComboBox();
+            kryptonComboBox4 = new Krypton.Toolkit.KryptonComboBox();
+            kryptonManager1 = new Krypton.Toolkit.KryptonManager(components);
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox1).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // kryptonComboBox3
+            // 
+            kryptonComboBox3.AlwaysActive = false;
+            kryptonComboBox3.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            kryptonComboBox3.CueHint.Color1 = Color.White;
+            kryptonComboBox3.CueHint.CueHintText = "Seleccionar";
+            kryptonComboBox3.Cursor = Cursors.Hand;
+            kryptonComboBox3.DropBackStyle = Krypton.Toolkit.PaletteBackStyle.ControlAlternate;
+            kryptonComboBox3.DropButtonStyle = Krypton.Toolkit.ButtonStyle.LowProfile;
+            kryptonComboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            kryptonComboBox3.FormattingEnabled = true;
+            kryptonComboBox3.InputControlStyle = Krypton.Toolkit.InputControlStyle.PanelAlternate;
+            kryptonComboBox3.Items.AddRange(new object[] { "asd", "asd", "sa", "das", "dsf" });
+            kryptonComboBox3.Location = new Point(3, 55);
+            kryptonComboBox3.Name = "kryptonComboBox3";
+            kryptonComboBox3.PaletteMode = Krypton.Toolkit.PaletteMode.MaterialDarkRipple;
+            kryptonComboBox3.Size = new Size(232, 30);
+            kryptonComboBox3.StateCommon.ComboBox.Back.Color1 = Color.FromArgb(55, 60, 72);
+            kryptonComboBox3.StateCommon.ComboBox.Border.Draw = Krypton.Toolkit.InheritBool.False;
+            kryptonComboBox3.StateCommon.ComboBox.Content.Color1 = Color.White;
+            kryptonComboBox3.StateCommon.ComboBox.Content.Font = new Font("Microsoft Sans Serif", 13.5F);
+            kryptonComboBox3.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonComboBox3.StateCommon.DropBack.Color1 = Color.FromArgb(45, 45, 45);
+            kryptonComboBox3.StateCommon.DropBack.Color2 = Color.FromArgb(10, 10, 10);
+            kryptonComboBox3.StateCommon.DropBack.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.None;
+            kryptonComboBox3.StateTracking.Item.Back.Color1 = Color.Crimson;
+            kryptonComboBox3.StateTracking.Item.Back.Color2 = Color.White;
+            kryptonComboBox3.TabIndex = 7;
+            // 
+            // kryptonComboBox1
+            // 
+            kryptonComboBox1.Location = new Point(241, 143);
+            kryptonComboBox1.Name = "kryptonComboBox1";
+            kryptonComboBox1.Size = new Size(8, 24);
+            kryptonComboBox1.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonComboBox1.TabIndex = 0;
+            kryptonComboBox1.Text = "kryptonComboBox1";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(kryptonComboBox2, 0, 1);
+            tableLayoutPanel1.Controls.Add(kryptonComboBox1, 1, 1);
+            tableLayoutPanel1.Controls.Add(kryptonComboBox3, 0, 0);
+            tableLayoutPanel1.Controls.Add(kryptonComboBox4, 1, 0);
+            tableLayoutPanel1.Location = new Point(35, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(476, 281);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // kryptonComboBox2
+            // 
+            kryptonComboBox2.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            kryptonComboBox2.AutoCompleteCustomSource.AddRange(new string[] { "sdf", "asdf", "asdf", "asdfasdf", "asd" });
+            kryptonComboBox2.CueHint.Color1 = Color.White;
+            kryptonComboBox2.CueHint.CueHintText = "Seleccionar";
+            kryptonComboBox2.CueHint.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonComboBox2.CueHint.TextV = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonComboBox2.Cursor = Cursors.Hand;
+            kryptonComboBox2.DropDownHeight = 250;
+            kryptonComboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            kryptonComboBox2.InputControlStyle = Krypton.Toolkit.InputControlStyle.PanelAlternate;
+            kryptonComboBox2.IntegralHeight = false;
+            kryptonComboBox2.Items.AddRange(new object[] { "asdf", "asdfsdaf", "ads", "fads", "fas", "dfsdf" });
+            kryptonComboBox2.ItemStyle = Krypton.Toolkit.ButtonStyle.Alternate;
+            kryptonComboBox2.Location = new Point(0, 198);
+            kryptonComboBox2.Margin = new Padding(0);
+            kryptonComboBox2.MaxDropDownItems = 25;
+            kryptonComboBox2.MaxLength = 300;
+            kryptonComboBox2.Name = "kryptonComboBox2";
+            kryptonComboBox2.PaletteMode = Krypton.Toolkit.PaletteMode.MaterialDarkRipple;
+            kryptonComboBox2.Size = new Size(238, 24);
+            kryptonComboBox2.StateActive.ComboBox.Back.Color1 = Color.FromArgb(55, 60, 72);
+            kryptonComboBox2.StateActive.ComboBox.Border.Width = 3;
+            kryptonComboBox2.StateActive.ComboBox.Content.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            kryptonComboBox2.StateActive.ComboBox.Content.Padding = new Padding(-1, 10, -1, 10);
+            kryptonComboBox2.StateActive.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonComboBox2.StateCommon.ComboBox.Back.Color1 = Color.FromArgb(55, 60, 72);
+            kryptonComboBox2.StateCommon.ComboBox.Border.Draw = Krypton.Toolkit.InheritBool.True;
+            kryptonComboBox2.StateCommon.ComboBox.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom;
+            kryptonComboBox2.StateCommon.ComboBox.Border.Width = 3;
+            kryptonComboBox2.StateCommon.ComboBox.Content.Font = new Font("Times New Roman", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            kryptonComboBox2.StateCommon.ComboBox.Content.Padding = new Padding(5, 1, -1, 2);
+            kryptonComboBox2.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonComboBox2.StateCommon.DropBack.Color1 = Color.Black;
+            kryptonComboBox2.StateCommon.DropBack.Color2 = Color.Black;
+            kryptonComboBox2.StateCommon.DropBack.ColorAlign = Krypton.Toolkit.PaletteRectangleAlign.Control;
+            kryptonComboBox2.StateCommon.Item.Back.Color1 = Color.Black;
+            kryptonComboBox2.StateCommon.Item.Back.Color2 = Color.Black;
+            kryptonComboBox2.StateCommon.Item.Border.Color1 = Color.Black;
+            kryptonComboBox2.StateCommon.Item.Border.Color2 = Color.Black;
+            kryptonComboBox2.StateNormal.ComboBox.Back.Color1 = Color.FromArgb(55, 60, 72);
+            kryptonComboBox2.StateTracking.Item.Back.Color1 = Color.Crimson;
+            kryptonComboBox2.StateTracking.Item.Back.Color2 = Color.White;
+            kryptonComboBox2.TabIndex = 6;
+            // 
+            // kryptonComboBox4
+            // 
+            kryptonComboBox4.Location = new Point(241, 3);
+            kryptonComboBox4.Name = "kryptonComboBox4";
+            kryptonComboBox4.Size = new Size(202, 24);
+            kryptonComboBox4.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonComboBox4.StateCommon.DropBack.Color1 = Color.White;
+            kryptonComboBox4.TabIndex = 8;
+            kryptonComboBox4.Text = "kryptonComboBox4";
+            // 
+            // kryptonManager1
+            // 
+            kryptonManager1.BaseFont = new Font("Times New Roman", 9F);
+            kryptonManager1.GlobalPaletteMode = Krypton.Toolkit.PaletteMode.Microsoft365BlackDarkMode;
+            kryptonManager1.ToolkitStrings.MessageBoxStrings.LessDetails = "L&ess Details...";
+            kryptonManager1.ToolkitStrings.MessageBoxStrings.MoreDetails = "&More Details...";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox1).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox4).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Krypton.Toolkit.KryptonComboBox kryptonComboBox1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Krypton.Toolkit.KryptonComboBox kryptonComboBox2;
+        private Krypton.Toolkit.KryptonComboBox kryptonComboBox4;
+        private Krypton.Toolkit.KryptonManager kryptonManager1;
+        private Krypton.Toolkit.KryptonComboBox kryptonComboBox3;
+    }
+}
