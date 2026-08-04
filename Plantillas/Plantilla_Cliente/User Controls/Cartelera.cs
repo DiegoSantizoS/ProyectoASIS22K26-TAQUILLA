0fc247689eb10e3d53eab1ebf76b364ab8a3cc9c
RadeonHeartz <carriazal@miumg.edu.gt>
2026-08-03 23:41:30 -0600

    User Control Reservas Mejorado para requerimientos

--- Plantillas/Plantilla_Cliente/User Controls/Cartelera.cs
+++ Plantillas/Plantilla_Cliente/User Controls/Cartelera.cs
@@ -97,6 +97,7 @@ namespace Plantilla_Cliente
                 Dgv_Cartelera.Columns["idPelicula"].Visible = false;
 
 
+
             }
         }
         /* fin de Codigo de Miguel David Contreras Jacinto con carnet: 0901-21-3878 en la
