59a70fcc722e208b6d055b2864d9999d69530ac2
Diana Mishel Loeiza Ramírez <dloeizar@miumg.edu.gt>
2026-08-05 19:56:49 -0600

    ¨Modificacion_metodos¨

--- Plantillas/Con_Admin/Conexion.cs
+++ Plantillas/Con_Admin/Conexion.cs
@@ -18,13 +18,20 @@ namespace Con_Admin
         private String user = "root";
         private String password = "root";
         private String pconexion;
+
+        // Datos de sesión usados por los triggers de bitácora para saber
+        // quién y desde qué equipo/ip se realizó cada acción
+        public static int SesionIdUsuario { get; set; }
+        public static int? SesionIdNombrePc { get; set; }
+        public static string SesionIp { get; set; }
+
         public Conexion()
         {
             pconexion = "Server=" + server +
                ";Database=" + datebase +
                ";Uid=" + user +
-               ";Pwd=" + password + ";"; ;
-
+               ";Pwd=" + password +
+               ";Allow User Variables=True;";
         }
 
         public MySqlConnection GetConnection()
@@ -34,6 +41,20 @@ namespace Con_Admin
                 connecion = new MySqlConnection(pconexion);
                 connecion.Open();
 
+                // Le avisamos a MySQL quién es el usuario activo, para que//MISHEL LOEIZA
+                // los triggers de bitácora sepan a quién atribuir la acción
+                if (SesionIdUsuario > 0)
+                {
+                    using (var cmd = new MySqlCommand(
+                        "SET @bitacora_usuario = @uid, @bitacora_pc = @pcid, @bitacora_ip = @ip;",
+                        connecion))
+                    {
+                        cmd.Parameters.AddWithValue("@uid", SesionIdUsuario);
+                        cmd.Parameters.AddWithValue("@pcid", (object)SesionIdNombrePc ?? DBNull.Value);
+                        cmd.Parameters.AddWithValue("@ip", SesionIp ?? "0.0.0.0");
+                        cmd.ExecuteNonQuery();
+                    }
+                }
             }
             return connecion;
         }
